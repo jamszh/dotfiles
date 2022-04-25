855d8dc0d501751ac5527c7b9370b4795e88be0f
jamesz96 <jamesz96.au@gmail.com>
2022-04-25 10:17:03 +1000

    move to nvchad

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,10 +1,31 @@
--- Plugins
-require('plugins._packer')
+local present, impatient = pcall(require, "impatient")
 
--- Looks
-require('modules._looks')
+if present then
+   impatient.enable_profile()
+end
 
--- Mappings/Settings
-require('modules._settings')
+local core_modules = {
+   "core.options",
+   "core.autocmds",
+   "core.mappings",
+}
 
--- LSP
+for _, module in ipairs(core_modules) do
+   local ok, err = pcall(require, module)
+   if not ok then
+      error("Error loading " .. module .. "\n\n" .. err)
+   end
+end
+
+-- non plugin mappings
+require("core.mappings").misc()
+
+-- check if custom init.lua file exists
+if vim.fn.filereadable(vim.fn.stdpath "config" .. "/lua/custom/init.lua") == 1 then
+   -- try to call custom init, if not successful, show error
+   local ok, err = pcall(require, "custom")
+   if not ok then
+      vim.notify("Error loading custom/init.lua\n\n" .. err)
+   end
+   return
+end
