c20781a9dcab630f64654939fe005e80baf33407
jamesz96 <jamesz96.au@gmail.com>
2022-06-10 11:21:16 +1000

    update nv term mappings

--- config/NvChad/mappings.lua
+++ config/NvChad/mappings.lua
@@ -1,8 +1,22 @@
 local M = {}
 
 M.disabled = {
-  ["gd"] = "",
-  ["gr"] = "",
+  n = {
+    ["gd"] = "",
+    ["gr"] = "",
+    ["<C-h>"] = "",
+    ["<C-l>"] = "",
+    ["<C-j>"] = "",
+    ["<C-k>"] = "",
+    ["<A-i>"] = "",
+    ["<A-v>"] = "",
+    ["<A-h>"] = "",
+  },
+  t = {
+    ["<A-i>"] = "",
+    ["<A-v>"] = "",
+    ["<A-h>"] = "",
+  }
 }
 
 M.telescope = {
@@ -11,6 +25,25 @@ M.telescope = {
     ["<C-f>"] = { "<cmd> Telescope live_grep <CR>", "   live grep" },
     ["gd"]    = { "<cmd> Telescope lsp_definitions <CR>", " " },
     ["gr"]    = { "<cmd> Telescope lsp_references<CR>", " " },
+  }
+}
+
+M.nvterm = {
+  n = {
+    ["\\t"] = {
+       function()
+          require("nvterm.terminal").toggle "float"
+       end,
+       "   toggle floating term",
+    }
+  },
+  t = {
+    ["\\t"] = {
+       function()
+          require("nvterm.terminal").toggle "float"
+       end,
+       "   toggle floating term",
+    }
   }
 }
 
