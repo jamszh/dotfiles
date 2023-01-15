bda643020bd2a491c73f0580fd05fc3befeac5dd
jamesz96 <jamesz96.au@gmail.com>
2023-01-15 12:34:11 +1100

    [nvim-migrate] Initial commit to migrate off NvChad
    '=' not '-'

---
+++ config/nvim/init.lua
@@ -0,0 +1,29 @@
+-- boostrap lazy.nvim --
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+
+require("lazy").setup("plugins", {
+  defaults = { lazy = true },
+  install = { colorscheme = { "tokyonight" } },
+  checker = { enabled = true },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "tutor",
+        "netrwPlugin"
+      }
+    }
+  }
+})
