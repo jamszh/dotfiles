d41985d92bbb6f0bbcac3986f60001ccb8382570
James Z <jamesz96.au@gmail.com>
2023-01-22 13:31:52 +1100

    add telescope mappings (#4)

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -12,6 +12,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("config.options")
+
 require("lazy").setup("plugins", {
   defaults = { lazy = true },
   install = { colorscheme = { "tokyonight" } },
