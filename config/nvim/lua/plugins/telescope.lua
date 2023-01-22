106313d55d8cc05df62614b81adcee975c79dd0d
jamesz96 <jamesz96.au@gmail.com>
2023-01-22 13:30:06 +1100

    add telescope mappings

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,62 @@
+local path = require("lib.path")
+
+function grep_wrap(builtin, opts)
+  local params = { builtin = builtin, opts = opts }
+  return function()
+    builtin = params.builtin
+    opts = params.opts
+    opts = vim.tbl_deep_extend("force", { cwd = path.get_root() }, opts or {})
+    if builtin == "files" then
+      if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
+        opts.show_untracked = true
+        builtin = "git_files"
+      else
+        builtin = "find_files"
+      end
+    end
+    require("telescope.builtin")[builtin](opts)
+  end
+end
+
+local keys = {
+  { "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
+  { "<leader>/", grep_wrap("live_grep"), desc = "Find in Files (Grep)" },
+  { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+  { "<leader><space>", grep_wrap("files"), desc = "Find Files (root dir)" },
+  -- find
+  { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+  { "<leader>ff", grep_wrap("files"), desc = "Find Files (root dir)" },
+  { "<leader>fF", grep_wrap("files", { cwd = false }), desc = "Find Files (cwd)" },
+  { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+  -- git
+  { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
+  { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "status" },
+  -- search
+  { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
+  { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+  { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+  { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
+  { "<leader>sd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
+  { "<leader>sg", grep_wrap("live_grep"), desc = "Grep (root dir)" },
+  { "<leader>sG", grep_wrap("live_grep", { cwd = false }), desc = "Grep (cwd)" },
+  { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+  { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
+  { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+  { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+  { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
+  { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
+  { "<leader>sw", grep_wrap("grep_string"), desc = "Word (root dir)" },
+  { "<leader>sW", grep_wrap("grep_string", { cwd = false }), desc = "Word (cwd)" },
+  { "<leader>uC", grep_wrap("colorscheme", { enable_preview = true }), desc = "Colorscheme with preview" },
+  {
+    "<leader>ss",
+    grep_wrap("lsp_document_symbols", {
+      symbols = { "Class", "Function", "Method", "Constructor", "Interface", "Module", "Struct", "Trait", "Field", "Property" },
+    }),
+    desc = "Goto Symbol",
+  },
+}
+
 local options = {
   vimgrep_arguments = {
     "rg",
@@ -48,11 +107,12 @@ local options = {
 local config = {
   "nvim-telescope/telescope.nvim",
   cmd = "Telescope",
+  keys = keys,
   dependencies = {
     "nvim-lua/plenary.nvim"
   },
   opts = {
-    defaults = options
+    defaults = options,
   }
 }
 
