064ac5b17d9a5eb4be5efe1f02768a3cdec94614
James Z <jamesz96.au@gmail.com>
2023-01-26 10:51:34 +1100

    Add Neo Tree (#7)
    * fix more options
    
    * add neo-tree

---
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,29 @@
+local config = {
+  "nvim-neo-tree/neo-tree.nvim",
+  cmd = "Neotree",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  keys = {
+    {
+      "<leader>fe",
+      function()
+          require("neo-tree.command").execute({ toggle = true, dir = require("lib.path").get_root() })
+      end,
+      desc = "Explorer NeoTree (root dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+  }
+}
+
+return { config }
