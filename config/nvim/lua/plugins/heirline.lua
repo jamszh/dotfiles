3a7edd4e94c6c1b59586be649516d0593999da5b
James Z <jamesz96.au@gmail.com>
2023-01-31 19:55:05 +1100

    Add and Setup Heirline.nvim Pt 1 (#10)
    * some base config for heirline
    
    * updates to statusline and winbar
    
    * global statusline NICE
    
    * remove unneeded statusline since we're using global now
    
    * update deps

---
+++ config/nvim/lua/plugins/heirline.lua
@@ -0,0 +1,26 @@
+local heirline = require("lib.heirline")
+
+local plugin = {
+  "rebelot/heirline.nvim",
+  event = "UiEnter",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
+  },
+  config = function()
+    local colors = require("tokyonight.colors").setup()
+    require("heirline").load_colors(colors)
+    local setup_config = {
+      statusline = {
+        heirline.build_statuslines()
+      },
+      winbar = {
+        heirline.build_file_block(),
+      },
+      tabline = {},
+      status_column = {},
+    }
+    require("heirline").setup(setup_config)
+  end
+}
+
+return { plugin }
