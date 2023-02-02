b8299e5395b5487090030151571c19c3f9dc5ff5
James Z <jamesz96.au@gmail.com>
2023-02-03 08:52:49 +1100

    catppuccin theme - delicious! (#16)
    * catppuccin theme - :kiss:
    
    * remove tokyonight

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,9 +1,12 @@
 return {
-  { "folke/tokyonight.nvim",
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd([[colorscheme catppuccin]])
     end,
+
   }
 }
