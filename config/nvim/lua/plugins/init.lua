a55289ea62c972fe5ced5036fce7bfc089f293bc
jamesz96 <jamesz96.au@gmail.com>
2023-02-02 20:56:56 +1100

    remove tokyonight

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,12 +1,5 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    config = function()
-      -- vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false,
