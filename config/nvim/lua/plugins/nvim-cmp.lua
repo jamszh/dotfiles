54089fcd12bf7afd6aaa5fd03df461b71a7849b8
jamesz96 <jamesz96.au@gmail.com>
2023-02-09 20:29:47 +1100

    disable first auto first selection for completion

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,9 +1,6 @@
 local config = function()
   local cmp = require("cmp")
   return {
-    completion = {
-      completeopt = "menu,menuone,noinsert",
-    },
     snippet = {
       expand = function(args)
         require("luasnip").lsp_expand(args.body)
@@ -14,7 +11,7 @@ local config = function()
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
       { name = "nvim_lsp" },
