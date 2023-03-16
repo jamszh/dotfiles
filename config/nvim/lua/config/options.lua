7d3ebb0d8c7ce44f04a0d63dddeebcce7c01572e
James Z <jamesz96.au@gmail.com>
2023-03-17 09:30:32 +1100

    Enable clipboard for WSL2 (#19)

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -13,4 +13,23 @@ opt.number = true
 opt.relativenumber = true
 
 opt.termguicolors = true
+opt.clipboard = "unnamedplus"
+
+if vim.fn.has("wsl") == 1 then
+  vim.api.nvim_exec([[
+    let g:clipboard = {
+      \   'name': 'WslClipboard',
+      \   'copy': {
+      \      '+': 'clip.exe',
+      \      '*': 'clip.exe',
+      \    },
+      \   'paste': {
+      \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      \   },
+      \   'cache_enabled': 0,
+      \ }]]
+    , true
+  )
+end
 
