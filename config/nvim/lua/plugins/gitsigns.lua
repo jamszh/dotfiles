ddc3ae90ad8e0dbaad687b16c100dce737faa23a
jamesz96 <jamesz96.au@gmail.com>
2023-03-29 16:43:17 +1100

    updates

---
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,19 @@
+local plugin = {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    on_attach = function(buffer)
+      gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
+
+      map("n", "<leader>gr", gs.reset_buffer, "Reset Buffer")
+      map("n", "<leader>gd", gs.diffthis, "Diff This")
+      map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
+    end,
+  }
+}
+
+return { plugin }
