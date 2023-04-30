96c3d7f8dbc46cd1c24397b87fd53781ad7fa903
James <jamesz96.au@gmail.com>
2023-05-01 08:43:59 +1000

    updates (#34)

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -5,6 +5,25 @@ local plugin = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
+    {
+      's1n7ax/nvim-window-picker',
+      config = function()
+        require'window-picker'.setup({
+          autoselect_one = true,
+          include_current = false,
+          filter_rules = {
+            -- filter using buffer options
+            bo = {
+              -- if the file type is one of following, the window will be ignored
+              filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+
+              -- if the buffer type is one of following, the window will be ignored
+              buftype = { 'terminal', "quickfix" },
+            },
+          },
+        })
+      end,
+    }
   },
   keys = {
     {
