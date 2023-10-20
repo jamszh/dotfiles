8c79ae64aea1a3b89a63798786a2393e738f8e1a
jamszh <jamesz96.au@gmail.com>
2023-10-20 18:07:48 +1100

    updates

--- config/nvim/lua/config/commands.lua
+++ config/nvim/lua/config/commands.lua
@@ -7,4 +7,10 @@ local function map(mode, lhs, rhs, opts)
 end
 
 vim.api.nvim_create_user_command("CopyRelPath", "call setreg('+', expand('%'))", {})
+
 map("n", "<leader>cp", "<cmd>lua require('lib/path').copy_path()<CR>", { silent=false, desc="Copy relative path" })
+
+map("n", "<C-u>", "<C-u>zz", { silent=false, desc = "Up page and re-centre" })
+map("n", "<C-d>", "<C-d>zz", { silent=false, desc = "Down page and re-centre" })
+map("n", "<C-i>", "<C-i>zz", { silent=false, desc = "Forward cursor and re-centre" })
+map("n", "<C-o>", "<C-o>zz", { silent=false, desc = "Backward cursor and re-centre" })
